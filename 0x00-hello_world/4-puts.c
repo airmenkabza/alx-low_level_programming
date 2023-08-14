f91a2d93039fa795c9ea856ff36539cdeebd6d4f
kabelo <modipakabelo8@gmail.com>
2023-08-14 09:36:58 +0200

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/** main - Entry point of the program
- *
+/*
+ * main - Entry point of the program 
  * This function prints a message using the puts function.
  *
  * Return: Always 0 (Success)
