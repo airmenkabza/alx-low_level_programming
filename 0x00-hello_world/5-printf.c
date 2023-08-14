f5eb7daa958f92b4e2297e7de6818dafc39bed37
kabelo <modipakabelo8@gmail.com>
2023-08-14 09:55:58 +0200

    updates

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,16 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints a message using the puts function.
+ * Description: Prints the sizes of various data types using the printf function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
