1ffee10c538028edf736358b315e27d5e5c24ea6
kabelo <modipakabelo8@gmail.com>
2023-08-14 09:59:45 +0200

    updates

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <unistd.h>
 #include <string.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main() {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, strlen(message));
-    return 1;
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, strlen(message));
+return (1);
 }
