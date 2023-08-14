f57a4a63c81185778b81daee1d6a57a44049723e
kabelo <modipakabelo8@gmail.com>
2023-08-14 09:32:24 +0200

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
-/**
- * main - Entry point of the program
- *
- * Description: Prints a message using the puts function.
- *
- * Return: Always 0 (Success)
- */
+//main - Entry point of the program
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
