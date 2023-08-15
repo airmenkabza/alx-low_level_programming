4f3856b599fff38e543cd400fbc3a77f14a4f8d6
kabelo <modipakabelo8@gmail.com>
2023-08-15 10:29:15 +0200

    updates

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -17,7 +17,7 @@ return (last_digit);
 int main(void)
 {
 int testNumbers[] = {-123, 0, 456, -789};
-int i;
+size_t i;
 for (i = 0; i < sizeof(testNumbers) / sizeof(testNumbers[0]); i++)
 {
 printf("Last digit: %d\n", print_last_digit(testNumbers[i]));
