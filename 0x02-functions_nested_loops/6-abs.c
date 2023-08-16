e325a2e3df229e44b3864965da640ffa3ea21566
kabelo <modipakabelo8@gmail.com>
2023-08-16 18:24:54 +0200

    updats

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_abs - Computes the absolute value of an integer.
- *The integer for which to compute the absolute value.
+ *_abs - Computes the absolute value
+ *The integer for which to compute
  *Return: The absolute value of n.
  */
 int _abs(int n)
@@ -11,14 +11,4 @@ if (n < 0)
 return (-n);
 else
 return (n);
-}
-int main(void)
-{
-int testValues[] = {-1, 0, 1, -98};
-size_t i;
-for (i = 0; i < sizeof(testValues) / sizeof(testValues[0]); i++)
-{
-printf("%d\n", _abs(testValues[i]));
-}
-return (0);
 }
