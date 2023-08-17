d4054cf4242cec68230c3255c5698ebfdca0422d
kabelo <modipakabelo8@gmail.com>
2023-08-17 11:43:56 +0200

    updates

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <unistd.h>
-int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
@@ -13,4 +12,5 @@ void more_numbers(void);
 void print_most_numbers(void);
 void print_square(int);
 void print_number(int);
+int _putchar(char);
 #endif /* MAIN_H */
