eb8588500bd569fab9ffddbd4a81d80bc87364e6
kabelo <modipakabelo8@gmail.com>
2023-08-17 11:37:16 +0200

    updates

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _isdigit(int);
 int mul(int, int);
 void print_numbers(void);
 void print_diagonal(int);
+void print_triangle(int);
 void print_line(int);
 void more_numbers(void);
 void print_most_numbers(void);
